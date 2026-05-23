3ae9081998303b0c6614da7333f156dc32312625
Tutul Majumder <tutul.majumder.11@gmail.com>
2026-05-23 23:37:42 +0600

    third commit

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,18 @@ import cors from "cors";
 const app: Application = express();
 
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "DevPulse API is running 🚀" });
+});
 app.use("/api/auth", userRoute);
 app.use("/api/issues", issueRoute);
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:8000",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 export default app;
